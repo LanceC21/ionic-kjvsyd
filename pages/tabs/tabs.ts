2733b7804aa35fb1190ba3f4e14a75cfa6c92f43
LanceC21 <lance.clemente080120@my.lsgh.edu.ph>
2021-01-22 02:33:45 +0800

    1 file modified, 3 added, 3 deleted

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { CalculatorPage } from '../calculator/calculator';
+import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = CalculatorPage;
+  tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
